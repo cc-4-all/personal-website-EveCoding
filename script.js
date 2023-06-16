ea9b3ee4b7fdc1fd4b31097a80fbb0ae6aa5bbae
jakewarrenblack <47800618+jakewarrenblack@users.noreply.github.com>
2023-06-16 13:00:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let pic = document.getElementById("profile-picture");
+var img = document.getElementById ("gallery-img");
+var prev = document.getElementById ("prev");
+var next = document.getElementById ("next");
 
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
-  }
-});
+var images = [Languages.jpg, Science.jpg, Art.jpg, Activism.jpg, Media.jpg]
+
+var currentimage = 0
+
+function changeimage(e) {
+if (e.target.id == "next" && currentimage !== images.length - 1)
+// increment the images (in other words, go to the next one)
+currentimage = currentimage + 1;
+}
+
+if (e.target.id == "prev" && currentimage !==0) {
+
+    currentimage = currentimage - 1;
+}
+
+images.setAttribute("src", "images/" + images [currentimage])
+}
+
+prev.addEventListener("click", changeimage)
+next.addEventListener("click", changeimage)
+
